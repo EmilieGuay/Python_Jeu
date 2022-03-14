f42c401aed5c5b4d49625a74e6e0d19168b49633
EmilieGuay <59967998+EmilieGuay@users.noreply.github.com>
2022-03-14 11:56:04 -0400

    Update test_functions.py

--- test_functions.py
+++ test_functions.py
@@ -1,11 +1,7 @@
-def square34(number):
-    return number*number
-
-
-
 import unittest
+
 class Samples(unittest.TestCase):
     def test(self):
-        self.assertEqual(square34(3), 9)
-
+        self.assertEqual(cubeA(3), 27)
+        self.assertRaises(TypeError, cubeA, "s")
         
