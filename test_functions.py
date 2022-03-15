39b91a10dc848e1d332e73f95b2c8f103984a14e
EmilieGuay <59967998+EmilieGuay@users.noreply.github.com>
2022-03-15 17:12:13 -0400

    Update test_functions.py

--- test_functions.py
+++ test_functions.py
@@ -2,6 +2,7 @@ import unittest
 
 class Samples(unittest.TestCase):
     def test(self):
+        global cubeA
         self.assertEqual(cubeA(3), 27)
         self.assertRaises(TypeError, cubeA, "s")
         self.assertRaises(TypeError, cubeA, 3j)
