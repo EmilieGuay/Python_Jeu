c6c63533dfabb69acb1e549aaa9fc9475431f658
EmilieGuay <59967998+EmilieGuay@users.noreply.github.com>
2022-03-15 13:31:58 -0400

    Update test_functions.py

--- test_functions.py
+++ test_functions.py
@@ -4,4 +4,4 @@ class Samples(unittest.TestCase):
     def test(self):
         self.assertEqual(cubeA(3), 27)
         self.assertRaises(TypeError, cubeA, "s")
-        
+        self.assertRaises(TypeError, cubeA, 3j)
